13b918e7d33311e54314c3a7daace9e153d2dfa0
p00ria <amir1300sobatdar@gmail.com>
2016-06-11 12:18:21 +0430

    Update Fantasy_Writer.lua

--- plugins/Fantasy_Writer.lua
+++ plugins/Fantasy_Writer.lua
@@ -1,6 +1,6 @@
 local function run(msg, matches)
-	if not is_owner(msg) then
-		return "فقط برای صاحب گروه" 
+	if not is_mmod(msg) then
+		return "فقط برای مدیران"
 	end
 	if #matches < 2 then
 		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
